4a7b42d8fd68431111f055d2c9acb433a4a52ba6
Santhoshkrishnachaitanya Chelikavada <everythingskc@gmail.com>
2017-08-11 01:02:54 -0400

    Finished configuring welcome message

--- app/src/main/java/com/everythingforward/dormmarket/activities/UserProfileActivity.java
+++ app/src/main/java/com/everythingforward/dormmarket/activities/UserProfileActivity.java
@@ -6,17 +6,28 @@ import android.widget.TextView;
 
 import com.everythingforward.dormmarket.R;
 import com.everythingforward.dormmarket.presenters.UserProfileActivityPresenter;
+import com.parse.ParseUser;
+import com.vstechlab.easyfonts.EasyFonts;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class UserProfileActivity extends AppCompatActivity {
 
     UserProfileActivityPresenter presenter;
 
-    TextView welcomeMessageTV;
+    @BindView(R.id.welcome_message) TextView welcomeMessageTV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        ButterKnife.bind(this);
         presenter = new UserProfileActivityPresenter();
+
+        welcomeMessageTV.setTypeface(EasyFonts.walkwaySemiBold(UserProfileActivity.this));
+        welcomeMessageTV.setText(presenter.getUserName(ParseUser.getCurrentUser()));
+
 
 
     }
