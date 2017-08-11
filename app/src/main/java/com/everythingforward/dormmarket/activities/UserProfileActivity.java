0e7c006d07d7cb3908fdfe22e4834d8983f3aa0d
Santhoshkrishnachaitanya Chelikavada <everythingskc@gmail.com>
2017-08-11 00:52:31 -0400

    Finished Toolbar for UserProfileActivity

--- app/src/main/java/com/everythingforward/dormmarket/activities/UserProfileActivity.java
+++ app/src/main/java/com/everythingforward/dormmarket/activities/UserProfileActivity.java
@@ -2,14 +2,22 @@ package com.everythingforward.dormmarket.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.everythingforward.dormmarket.R;
+import com.everythingforward.dormmarket.presenters.UserProfileActivityPresenter;
 
 public class UserProfileActivity extends AppCompatActivity {
 
+    UserProfileActivityPresenter presenter;
+
+    TextView welcomeMessageTV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_market);
+        setContentView(R.layout.activity_user_profile);
+        presenter = new UserProfileActivityPresenter();
+
+
     }
 }
